34d738ea9934d59b0571034a126288db1ab16906
WCPE <1837019522@qq.com>
2024-05-17 14:51:07 +0800

    fix: 修复 openGui 指令执行会打印 help

--- src/main/kotlin/top/wcpe/dragoncoregui/DragonCoreGui.kt
+++ src/main/kotlin/top/wcpe/dragoncoregui/DragonCoreGui.kt
@@ -176,6 +176,8 @@ class DragonCoreGui : JavaPlugin() {
                 if (arg1 == "openGui") {
                     val playerExact = getPlayer(sender, arg2) ?: return
                     coreManager.openGui(playerExact, arg3)
+                    sender.sendMessage("已发送界面 [$arg3] 打开请求至玩家 [$playerExact] !")
+                    return
                 }
                 if (arg1 == "document") {
                     if (arg2 == "packet") {
